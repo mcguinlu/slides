df16003d657b4a8e3b0fa293b6f2e74151e45245
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2019-09-09 15:53:19 +0100

    Updates

--- app-1/app.R
+++ app-1/app.R
@@ -1,48 +1,24 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(BristolVis)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+      shiny::selectInput(inputId = "fill", 
+                            label = "Variable to fill by:",
+                            choices = c("Health" = "health",
+                                        "Treatment" = "treatment")),
+      plotOutput("barPlot")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+   output$barPlot <- renderPlot({
+      ggplot(data = med, aes(fill = get(input$fill))) +
+         geom_histogram(aes(x = status), stat = "count") +
+         labs(fill = "Fill")
+      })
 }
 
 # Run the application 
