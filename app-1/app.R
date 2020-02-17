315d1615f410f19fd863063dbe1c6b167f06f0f4
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2020-02-17 20:14:21 +0000

    Update app

--- app-1/app.R
+++ app-1/app.R
@@ -1,6 +1,7 @@
 library(shiny)
-library(BristolVis)
 library(ggplot2)
+
+bmi2 <- read.csv("http://bit.ly/phd-intro-bmi2")
 
 ui <- fluidPage(
    sidebarLayout(
@@ -8,19 +9,21 @@ ui <- fluidPage(
          selectInput(
             inputId = "fill",
             label = "Variable to fill by:",
-            choices = c("Health" = "health",
-                        "Treatment" = "treatment"))
+            choices = c("Health status" = "status",
+                        "Diet" = "diet"))
       ),
-      mainPanel(
-            plotOutput("barPlot")
-            )
-)
+      
+      mainPanel(plotOutput("barPlot"))
+      
+   )
 )
 
 server <- function(input, output) {
    output$barPlot <- renderPlot({
-      ggplot(data = med, aes_string(fill = input$fill)) +
-         geom_histogram(aes(x = status), stat = "count") +
+      ggplot(data = bmi2) +
+         aes(x = sex) +
+         aes_string(fill = input$fill) +
+         geom_histogram(stat = "count") +
          labs(fill = "Fill")
    })
 }
